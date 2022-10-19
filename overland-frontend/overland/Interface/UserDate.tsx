6ced9b6510312141f021feb2050d3c9e233acdf9
MayBourdal <maybourdal6@gmail.com>
2022-10-19 15:19:15 -0300

    avances front

---
+++ overland-frontend/overland/Interface/UserDate.tsx
@@ -0,0 +1,12 @@
+export interface UserData{
+    id: number,
+    name: string,
+    dni: number,
+    gender: string,
+    lastName: string,
+    nationality: string,
+    phone: number,
+    pathologies: string,
+    born: string,
+    obserLunch: string,
+   }
